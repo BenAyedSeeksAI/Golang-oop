a465d423ca0f3a7d1dc97dac31bc1e5a2e9a2a63
Mohamed Fares Ben ayed <faresaction@yahoo.fr>
2022-09-11 17:19:07 +0200

    add args show and add

--- main.go
+++ main.go
@@ -3,28 +3,36 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
 func instanceCalculate(list [][]string) []Calculate {
-	var ElementCalculate Calculate
 	var listCalculate []Calculate
 	for _, line := range list {
 		age, _ := strconv.Atoi(line[2])
 		Salary, _ := strconv.Atoi(line[3])
 		Bonus, _ := strconv.Atoi(line[4])
+		var ElementCalculate Calculate
 		if line[0] == "Engineer" {
 			ElementCalculate = Engineer{
-				details: Employee{Name: line[1], Age: age, Salary: Salary, Bonus: Bonus},
+				details: Employee{Name: line[1],
+					Age:    age,
+					Salary: Salary,
+					Bonus:  Bonus},
 			}
 		} else if line[0] == "Technician" {
 			ElementCalculate = Technician{
-				details: Employee{Name: line[1], Age: age, Salary: Salary, Bonus: Bonus},
+				details: Employee{Name: line[1],
+					Age:    age,
+					Salary: Salary,
+					Bonus:  Bonus},
 			}
 		}
 		listCalculate = append(listCalculate, ElementCalculate)
+
 	}
 	return listCalculate
 }
@@ -40,19 +48,47 @@ func readFileData(fileName string) [][]string {
 
 	list := make([][]string, 0)
 	for fileScanner.Scan() {
-		Element := strings.Split(fileScanner.Text(), " ")
-		list = append(list, Element)
+		if fileScanner.Text() != "" {
+			Element := strings.Split(fileScanner.Text(), " ")
+			list = append(list, Element)
+		}
 	}
 	readFile.Close()
 	return list
 }
-
-func main() {
+func show() {
 	var fileName string = "data.txt"
 	list := readFileData(fileName)
 	listOfSalaries := instanceCalculate(list)
 	for _, element := range listOfSalaries {
 		fmt.Println(element.getProfession())
 		fmt.Println(element.annualSalary())
+	}
+}
+func getArgs() string {
+	return os.Args[1]
+}
+func add() {
+	fmt.Println("Enter a whole line of data :")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+	fmt.Printf("%T", input)
+	// open file
+	file, err := os.OpenFile("data.txt", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// write the input
+	defer file.Close()
+	file.WriteString(input + "\n")
+}
+
+func main() {
+	Argument := getArgs()
+	if Argument == "add" {
+		add()
+	} else if Argument == "show" {
+		show()
 	}
 }
